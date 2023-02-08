0e5779f024f50805e01b2d7f820bb46c803defc0
Mohamad Zachroy Anazfitry <zachroy.anazfitry@pipeline-network.com>
2023-02-08 17:23:57 +0800

    pengurusan-pengguna

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,11 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('pendaftaran')" :active="request()->routeIs('daftar')">
+                        {{ __('Pendaftaran Pengguna') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
